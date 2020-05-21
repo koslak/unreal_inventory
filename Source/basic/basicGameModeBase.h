4356c50d9b2bc4c7b04c54e64e1e431ee51bf08e
Koslak <luis@cas3ai.com>
2020-05-21 18:14:03 -0500

    First commit of this project.

---
+++ Source/basic/basicGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "basicGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASIC_API AbasicGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
